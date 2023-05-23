c4c24a2d46fa83a55bd633c261c3c735ffc6d86d
Princewill Chiaka <9r1nc3w1ll@gmail.com>
2023-05-24 00:29:38 +0100

    chore: Get backend url from env

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,22 +1,23 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
- 
+
 export async function middleware(request: NextRequest) {
   interface TAppStatus {
-   step1: boolean;
-   step2: boolean;
+    step1: boolean;
+    step2: boolean;
   }
 
+  const initCheckUrl = `${process.env.BACKEND_URL ?? 'http://localhost:8000'}/school/setup-status`
   const step1Path = '/onboarding/step1';
   const step2Path = '/onboarding/step2';
   const loginPath = '/login';
 
-  // Allow /_next and assets to get through
-  if (request.nextUrl.pathname.match(/(^\/_next)|(\.(png)|(ico)|(svg)|(json)|(jp(e)?g)$)/)) {
+  // Allow step1, /_next and assets to get through
+  if (request.nextUrl.pathname.startsWith(step1Path) || request.nextUrl.pathname.match(/(^\/_next)|(\.(png)|(ico)|(svg)|(json)|(jp(e)?g)$)/)) {
     return;
   }
 
-  const response = await fetch("http://127.0.0.1:8000/school/status", { method: "GET"});
+  const response = await fetch(initCheckUrl, { method: "GET" });
   if (response.statusText.toLocaleLowerCase() !== 'ok') {
     // TODO: We should redirect to technical setup page or 500
     console.warn('backend api failed', { response });
@@ -30,16 +31,16 @@ export async function middleware(request: NextRequest) {
     console.log('redirecting', { pathname: request.nextUrl.pathname, destination: step1Path });
     return NextResponse.redirect(new URL(step1Path, request.url));
   }
-  
+
   if (status.step1 && !status.step2 && !request.nextUrl.pathname.startsWith(step2Path)) {
-    console.log('redirecting', { pathname: request.nextUrl.pathname, destination:  step2Path });
+    console.log('redirecting', { pathname: request.nextUrl.pathname, destination: step2Path });
     return NextResponse.redirect(new URL(step2Path, request.url));
   }
 
   if (
     status.step1 &&
     status.step2 &&
-    (request.nextUrl.pathname.startsWith(step1Path) || request.nextUrl.pathname.startsWith(step2Path) )
+    (request.nextUrl.pathname.startsWith(step1Path) || request.nextUrl.pathname.startsWith(step2Path))
   ) {
     return NextResponse.redirect(new URL(loginPath, request.url));
   }
