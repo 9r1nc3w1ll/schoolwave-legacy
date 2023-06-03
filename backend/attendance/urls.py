22cb4b2c52ba707588fe563df62126731c1bdd78
danielzion <danielolaniyan1@gmail.com>
2023-06-03 11:27:17 +0100

    Studnet Attendance Test

--- backend/attendance/urls.py
+++ backend/attendance/urls.py
@@ -3,11 +3,6 @@ from django.urls import path
 from .views import ListCreateStudentAttendance, RetrieveUpdateDestoryStudentAttendance
 
 urlpatterns = [
-<<<<<<< HEAD
     path('student-attendance', ListCreateStudentAttendance.as_view(), name='student_attendance_list_create'),
     path('student-attendance/<int:pk>/', RetrieveUpdateDestoryStudentAttendance.as_view(), name='student_attendance_retrieve_update_destroy'),
-=======
-    path('student-attendance', StudentAttendanceListCreateAPIView.as_view(), name='student-attendance-list-create'),
-    path('student-attendance/<int:pk>/', StudentAttendanceRetrieveUpdateDestroyAPIView.as_view(), name='student-attendance-retrieve-update-destroy'),
->>>>>>> 6a8c99d (Student Attendance Testing)
 ]
