3c6730ccfed3adeac21bd37b797ac91b7987c477
Princewill Chiaka <9r1nc3w1ll@gmail.com>
2023-06-12 00:37:58 +0100

    ci: Use env group

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY", "secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
@@ -192,6 +192,6 @@ LOGGING = {
     },
 }
 
-FRONTEND_URL = config("FRONTEND_URL")
+FRONTEND_URL = config("FRONTEND_URL", "localhost:3000")
 
 SITE_ID = 1
