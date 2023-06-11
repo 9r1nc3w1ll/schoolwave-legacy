2aaa5a78f5afa45c7f6144ce56a524b381dfa3a9
Princewill Chiaka <9r1nc3w1ll@gmail.com>
2023-06-11 16:18:43 +0100

    ci: Add database

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -132,9 +132,9 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": config("DATABASE_NAME"),
         "USER": config("DATABASE_USER"),
+        "HOST": config("DATABASE_HOST", "localhost"),
+        "PORT": config("DATABASE_PORT", "5432"),
         "PASSWORD": config("DATABASE_PASSWORD"),
-        "HOST": "localhost",
-        "PORT": "5432",
     }
 }
 
