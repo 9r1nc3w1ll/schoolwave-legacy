30f0e60a5d853949f8bf55db4ef1e2fd800c6ead
Kayode <khaox07@gmail.com>
2023-06-23 08:19:52 +0100

    Added new role; return student name in admissions

--- backend/admission/serializers.py
+++ backend/admission/serializers.py
@@ -3,6 +3,12 @@ from .models import AdmissionRequest, StudentInformation
 
 
 
+class StudentNameSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = StudentInformation
+        fields = ("id", "name")
+
 class StudentInformationSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -10,6 +16,7 @@ class StudentInformationSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 class AdmissionRequestSerializer(serializers.ModelSerializer):
+    student_info = StudentNameSerializer()
     class Meta:
         model = AdmissionRequest
         fields = "__all__"
