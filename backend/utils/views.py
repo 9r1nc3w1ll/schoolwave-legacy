321e4bfe21f3a5846f82312d2fac199a514a714c
Kayode <khaox07@gmail.com>
2023-06-27 16:18:37 +0100

    Updated PR

--- backend/utils/views.py
+++ backend/utils/views.py
@@ -1,4 +1,3 @@
-from .webhook import BaseWebhook
 from django.db.models import F
 from django.db import transaction
 
@@ -6,13 +5,23 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+import hashlib
+import hmac
+from django.conf import settings
+
 
 class PaystackWebhook(APIView):
     def post(self, request, *args, **kwargs):
-        webhook_data, webhook_status = BaseWebhook(request).webhook_handler()
+        secret = getattr(settings, "PAYSTACK_SECRET_KEY")
 
-        if not webhook_status:
+        data = request.body
+        hash = hmac.new(key=secret.encode('utf-8'), msg=data, digestmod=hashlib.sha512).hexdigest()
+        
+        if hash != request.headers["x-paystack-signature"]:
             return Response({"error" : "Paystack Validation Failed"}, status=status.HTTP_400_BAD_REQUEST)
+
+        webhook_data = request.data
+            
         
         if webhook_data["event"] == "charge.success":
             # Handle payment here when fleshed out.
