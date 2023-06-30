60385cb78a1767ac8300e2a3891817e0e8678765
ALPHAWGA BAMIDELE <alphawgas@gmail.com>
2023-06-30 06:46:10 +0100

    Done all

--- frontend/components/FeesList.tsx
+++ frontend/components/FeesList.tsx
@@ -1,4 +1,6 @@
-
+import FormGroup from '@mui/material/FormGroup';
+import { FormControlLabel } from '@mui/material';
+import { Switch } from '@mui/material';
 
 const ParentLogin = () => {
 
@@ -8,7 +10,7 @@ const ParentLogin = () => {
         <div className="panel">
                     <h5 className="mb-5 text-lg font-semibold dark:text-white-light">Login details</h5>
 
-                    <form className="space-y-5">
+                    <FormGroup className="space-y-5">
                         <div>
                             <label htmlFor="groupFname">First Name</label>
                             <input id="groupFname" type="text" placeholder="Enter First Name" className="form-input" />
@@ -25,11 +27,20 @@ const ParentLogin = () => {
                             <label htmlFor="groupLname">Password</label>
                             <input id="groupLname" type="password" placeholder="Enter Last Name" className="form-input" />
                         </div>
-                       
+                        <div className=' flex gap-5 justify-items-stretch'>
                         <button type="button" className="btn btn-primary !mt-6">
                             Submit
                         </button>
-                    </form>
+
+                        <FormControlLabel control={<Switch defaultChecked />} label="Disable" className='justify-end  w-full mt-5' />
+
+                        </div>
+                       
+                       
+
+                        
+
+                    </FormGroup>
        
 
 </div>
