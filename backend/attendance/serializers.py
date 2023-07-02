bb1792af26d06374a83dbce970939de056f22b40
danielzion <danielolaniyan1@gmail.com>
2023-07-02 20:41:57 +0100

    Student Attendance Response Details

--- backend/attendance/serializers.py
+++ backend/attendance/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import AttendanceRecord
-# from .models import StudentInformation
 
 class AttendanceRecordSerializer(serializers.ModelSerializer):
     student = serializers.SerializerMethodField()
