5d0649d5e0b717ae954d023c2d4970267b4c3fa0
woleekanola <woleekanola@gmail.com>
2023-07-07 09:23:52 +0100

    admin dash board page

---
+++ frontend/pages/dashboard/index.tsx
@@ -0,0 +1,11 @@
+
+
+const Dashboard =()=>{
+
+
+  return(
+    <h1> Welcome to schoolwave admin Dashboard</h1>
+  )
+}
+
+export default Dashboard
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -86,7 +86,7 @@ const Index = () => {
                     type='button'
                     className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
-                    <Link href='/class'>Go to Dashboard</Link>
+                    <Link href='/dashboard'>Go to Dashboard</Link>
                   </button>
                 </div>}
                 <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
