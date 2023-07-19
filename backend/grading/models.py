fada1b67eb0b8a7e6622bee2485da30c6a66cb6c
Kayode <khaox07@gmail.com>
2023-07-19 07:15:06 +0100

    Updated grading

--- backend/grading/models.py
+++ backend/grading/models.py
@@ -11,11 +11,15 @@ from subject.models import Subject
 
 PERCENTAGE_VALIDATOR = [MinValueValidator(0), MaxValueValidator(100)]
 
+# TOTAL_SCORE: 100
+
 class Grade(BaseModel):
     weight = models.IntegerField(validators=PERCENTAGE_VALIDATOR)
     student = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     term = models.ForeignKey(Term, on_delete=models.CASCADE)
+    score = models.DecimalField(max_digits=5, decimal_places=2)
+    
 
 
 class GradingScheme(BaseModel):
@@ -33,13 +37,15 @@ class Result(BaseModel):
     term = models.ForeignKey(Term, on_delete=models.CASCADE)
     total_score = models.DecimalField(max_digits=5, decimal_places=2)
     grade = models.CharField(max_length=255)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
 
     def compute_result(self):
         grading_scheme = GradingScheme.objects.get(school=self.student.school)
 
-        grades = Grade.objects.filter(term=self.term, student=self.student)
+        grades = Grade.objects.filter(term=self.term, student=self.student, subject=self.subject)
 
-        weighted_total = grades.aggregate(total=Sum('weight'))['total']
+
+        weighted_total = grades.aggregate(total=Sum('score'))['total']
 
         if weighted_total is not None:
             scheme = grading_scheme.scheme
