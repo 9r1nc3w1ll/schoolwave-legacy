3e73ea7ae0d83d50f7848d04e7d0bbb8d98601fd
ALPHAWGA BAMIDELE <alphawgas@gmail.com>
2023-07-24 16:20:12 +0100

    exam bug fix

--- frontend/pages/exams/index.tsx
+++ frontend/pages/exams/index.tsx
@@ -294,13 +294,13 @@ const Export = (props:any) => {
           <h5 className=" text-3xl font-semibold dark:text-white-light">Examinations</h5>
           <div className="flex flex-wrap items-center">
            
-            <button type="button" onClick={() => setuploadModal(true)} className="btn btn-primary btn-sm m-1">
+            {/* <button type="button" onClick={() => setuploadModal(true)} className="btn btn-primary btn-sm m-1">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 mr-2">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15" />
               </svg>
 
                             Bulk Examinations
-            </button>
+            </button> */}
      
             <button type="button"  className="btn btn-primary btn-sm m-1" onClick={()=>{
               setmodal(true)
