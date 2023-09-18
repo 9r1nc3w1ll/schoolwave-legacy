9373c428607d656bfb74be0ec1067e8ca8abaffe
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-09-18 19:36:58 +0100

    deletion of custom formatter

--- frontend/pages/admin_dashboard/index.tsx
+++ frontend/pages/admin_dashboard/index.tsx
@@ -313,10 +313,6 @@ const Dashboard = (props: any) => {
                             total: {
                                 show: true,
                                 label: 'Total',
-                                formatter: function (w: any) {
-                                    // By default this function returns the average of all series. The below is just an example to show the use of custom formatter function
-                                    return 249;
-                                },
                             },
                         },
                     },
