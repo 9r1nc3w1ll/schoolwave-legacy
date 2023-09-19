2fe1f52b725c46d84f51f4ceb74dbf5567d08e1c
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2023-09-19 04:41:15 +0100

    chore: update create school page | roles

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -6,42 +6,61 @@ import { signIn } from 'next-auth/react';
 import { AuthenticationRoute } from '@/components/Layouts/AuthenticationRoute';
 import { showAlert } from '@/utility_methods/alert';
 
-
 const LoginBoxed = () => {
-  const router = useRouter()
+  const router = useRouter();
 
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const onSubmit = async (data: any) => {
-    const result: any = await signIn('credentials', { username: data.email, password: data.password, redirect: false, callbackUrl: '/' })
+    const result: any = await signIn('credentials', {
+      username: data.email,
+      password: data.password,
+      redirect: false,
+      callbackUrl: '/',
+    });
     if (result.ok) {
-      showAlert('success', 'Logged in Successfuly')
-      router.push('/')
+      showAlert('success', 'Logged in Successfuly');
+      router.push('/');
       return;
-    }else{
-      showAlert('error', 'An error occured')
+    } else {
+      showAlert('error', 'An error occured');
     }
   };
 
   return (
     <AuthenticationRoute>
       <div className="flex min-h-screen items-center justify-center bg-[url('/assets/images/map.svg')] bg-cover bg-center dark:bg-[url('/assets/images/map-dark.svg')]">
-        <div className="panel m-6 w-full max-w-lg sm:w-[480px]">
-          <h2 className="mb-3 text-2xl font-bold">Sign In</h2>
-          <p className="mb-7">Enter your email and password to login</p>
-          <form className="space-y-5" onSubmit={handleSubmit(onSubmit)}>
+        <div className='panel m-6 w-full max-w-lg sm:w-[480px]'>
+          <h2 className='mb-3 text-2xl font-bold'>Sign In</h2>
+          <p className='mb-7'>Enter your email and password to login</p>
+          <form className='space-y-5' onSubmit={handleSubmit(onSubmit)}>
             <div>
-              <label htmlFor="email">Email</label>
-              <input id="email" type="email" className="form-input" placeholder="Enter Email"  {...register("email", { required: true, maxLength: 80 })} />
+              <label htmlFor='email'>Email/Username</label>
+              <input
+                id='email'
+                type='text'
+                className='form-input'
+                placeholder='Enter Email'
+                {...register('email', { required: true, maxLength: 80 })}
+              />
             </div>
             <div>
-              <label htmlFor="password">Password</label>
-              <input id="password" type="password" className="form-input" placeholder="Enter Password"  {...register("password", { required: true, maxLength: 80 })} />
+              <label htmlFor='password'>Password</label>
+              <input
+                id='password'
+                type='password'
+                className='form-input'
+                placeholder='Enter Password'
+                {...register('password', { required: true, maxLength: 80 })}
+              />
             </div>
-            <button type="submit" className="btn btn-primary w-full">
+            <button type='submit' className='btn btn-primary w-full'>
               SIGN IN
             </button>
           </form>
-
         </div>
       </div>
     </AuthenticationRoute>
